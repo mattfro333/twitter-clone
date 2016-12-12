e3681da58978400b4fa45a3c62a41bfb0a16318c
mattfro333 <mattfro333@gmail.com>
2016-12-12 10:07:01 -0700

    jquery

--- js/animations.js
+++ js/animations.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
 
 $(".button").hide();
+$("#char-count").hide();
 
+$(".tweet-compose").click(function(){
+$(".button").show();
+$("#char-count").show();
+$('.tweet-compose').show(function () {
+    $(this).animate({ height: "5em" }, 0);
 
+});
 
-
+});
 
 });
